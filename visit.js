aeccb309b6d06f03ff5173a0dfc5fc332fa9e217
Novan <n45pnasp@gmail.com>
2025-08-28 21:38:15 +0700

    chore: update visitor worker endpoint

--- visit.js
+++ visit.js
@@ -1,7 +1,7 @@
 import { requireAuth, getFirebase } from "./auth-guard.js";
 import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/9.22.2/firebase-auth.js";
 
-const SCRIPT_URL   = "https://logvisit.avsecbwx2018.workers.dev/";
+const SCRIPT_URL   = "https://pasvisit.avsecbwx2018.workers.dev/";
 const LOOKUP_URL   = "https://script.google.com/macros/s/AKfycbzgWQVOzC7cQVoc4TygW3nDJ_9iejZZ_4CBAWBFDrEXvjM5QxZvEiFr4FLKIu0bqs0Hfg/exec";
 const SHARED_TOKEN = "N45p";
 
